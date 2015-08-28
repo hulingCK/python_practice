053a0c7c38ad8670edfd2abd33655ccbc283f181
hulingCK <853918324@qq.com>
2015-08-28 10:24:42 +0800

    new ip

--- testpAC/first.py
+++ testpAC/first.py
@@ -1,4 +1,4 @@
-# coding:uft-8
+# coding:utf-8
 # 确定默认编码格式不能少
 __author__ = 'huling'
 
@@ -10,11 +10,11 @@ import time
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 DesiredCapabilities.INTERNETEXPLORER['ignoreProtectedModeSettings'] = True
 driver = webdriver.Ie()
-driver.get("http://10.66.62.39:8080/wms/")
+driver.get("http://10.66.62.88:8080/wms/")
 driver.find_element_by_id("username").clear()
-driver.find_element_by_id("username").send_keys("admin")
+driver.find_element_by_id("username").send_keys("superadmin")
 driver.find_element_by_name("password").clear()
-driver.find_element_by_name("password").send_keys(123456)
+driver.find_element_by_name("password").send_keys(111)
 print driver.find_element_by_name("password").text
 driver.find_element_by_xpath("//input[@class='btn btn-primary btn-block']").click()
 time.sleep(2)
@@ -26,3 +26,4 @@ else:
     print "wrong"
 
 driver.quit()
+print "test"
